55d4b92c6645b3be1312c322773f294d426974a6
Omkar patil <omkarp666@gmail.com>
2022-10-07 23:47:03 +0530

    Render-comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, {Component, useState} from "react";
+import ProjectList from "./ProjectList";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <div className="App">
+     <ProjectList name="html" description="html project with e-commerce"/>
+     <ProjectList name="css" description="css project with e-commerce"/>
+     <ProjectList name="javsacript" description="javascript project with e-commerce"/>
+     <ProjectList name="reactjs" description="reactjs project with e-commerce"/>
+    </div>
+    </div>
   )
 }
 
